31a265e80ec686e354ecc76f1f239139af65bba5
Quentin Hausser <hausser.quentin@gmail.com>
2017-06-13 23:04:06 +0200

    First release of rxjs-wrapper

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,13 @@
-"use strict";
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _wrapper = require('./wrapper');
+
+var _wrapper2 = _interopRequireDefault(_wrapper);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = _wrapper2.default;
---
+++ dist/wrapper.js
@@ -0,0 +1,90 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _defineProperty2 = require('babel-runtime/helpers/defineProperty');
+
+var _defineProperty3 = _interopRequireDefault(_defineProperty2);
+
+var _extends3 = require('babel-runtime/helpers/extends');
+
+var _extends4 = _interopRequireDefault(_extends3);
+
+var _classCallCheck2 = require('babel-runtime/helpers/classCallCheck');
+
+var _classCallCheck3 = _interopRequireDefault(_classCallCheck2);
+
+var _createClass2 = require('babel-runtime/helpers/createClass');
+
+var _createClass3 = _interopRequireDefault(_createClass2);
+
+var _ajax = require('rxjs/observable/dom/ajax');
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var RxjsWrapper = function () {
+  function RxjsWrapper(apiDefs) {
+    var store = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : function () {
+      return null;
+    };
+    (0, _classCallCheck3.default)(this, RxjsWrapper);
+
+    this.store = store;
+    this.apiDefs = apiDefs;
+    this.routes = [];
+
+    this.buildUrl = this.buildUrl.bind(this);
+    this.defBuilder = this.defBuilder.bind(this);
+  }
+
+  (0, _createClass3.default)(RxjsWrapper, [{
+    key: 'buildUrl',
+    value: function buildUrl(url, urlParams, queryParams) {
+      // eslint-disable-line
+      var finalUrl = url;
+      Object.keys(urlParams).forEach(function (param) {
+        finalUrl = finalUrl.replace(':' + param, urlParams[param]);
+      });
+      if (queryParams.constructor === Object && Object.keys(queryParams).length > 0) {
+        finalUrl = finalUrl.concat('?', Object.keys(queryParams).map(function (key) {
+          return encodeURIComponent(key) + '=' + encodeURIComponent(queryParams[key]);
+        }));
+      } else if (queryParams.constructor === String) {
+        finalUrl = finalUrl.concat('?', queryParams);
+      }
+      return finalUrl;
+    }
+  }, {
+    key: 'defBuilder',
+    value: function defBuilder(def, urlParams, body, queryParams) {
+      return {
+        url: this.buildUrl(def.url, urlParams, queryParams),
+        method: def.method,
+        headers: def.headers ? def.headers(this.store) : null,
+        responseType: def.responseType,
+        body: body
+      };
+    }
+  }, {
+    key: 'init',
+    value: function init() {
+      var _this = this;
+
+      var routes = {};
+      Object.keys(this.apiDefs).forEach(function (key) {
+        routes = (0, _extends4.default)({}, routes, (0, _defineProperty3.default)({}, '' + key, function undefined() {
+          var urlParams = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+          var body = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : null;
+          var queryParams = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : {};
+          return (0, _ajax.ajax)(_this.defBuilder(_this.apiDefs[key], urlParams, body, queryParams));
+        }));
+      });
+      this.routes = routes;
+    }
+  }]);
+  return RxjsWrapper;
+}();
+
+exports.default = RxjsWrapper;
