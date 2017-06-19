ae427ffe14d8acbee6f6497924ebfdf926ec737c
Quentin Hausser <hausser.quentin@gmail.com>
2017-06-19 23:24:42 +0200

    Debugging commut

--- src/library.js
+++ src/library.js
@@ -1,6 +1,7 @@
 const combineWrappers = (wrappers) => {
   let wrapped = {};
   Object.keys(wrappers).forEach((key) => {
+    console.log(key, wrappers[key]);
     wrapped = { [key]: wrappers[key].routes, ...wrapped };
   });
   return wrapped;
