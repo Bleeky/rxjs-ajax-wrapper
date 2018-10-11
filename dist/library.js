1440f28d6e6c616ed36621d32d7bf67751c37d30
Quentin <hausser.quentin@gmail.com>
2018-10-11 17:13:14 +0200

    Adding new wrapper

--- dist/library.js
+++ dist/library.js
@@ -12,6 +12,10 @@ var _extends3 = require("babel-runtime/helpers/extends");
 
 var _extends4 = _interopRequireDefault(_extends3);
 
+var _toConsumableArray2 = require("babel-runtime/helpers/toConsumableArray");
+
+var _toConsumableArray3 = _interopRequireDefault(_toConsumableArray2);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function CombineWrappers(wrappers) {
@@ -22,25 +26,57 @@ function CombineWrappers(wrappers) {
 
   this.resources = {};
   this.wrappers = wrappers;
+  this.requestMiddlewares = { middlewares: [], params: undefined };
+  this.errorMiddlewares = { middlewares: [], params: undefined };
   Object.keys(this.wrappers).forEach(function (key) {
     if (reqMdw.reqMdw) {
+      _this.requestMiddlewares = {
+        middlewares: [].concat((0, _toConsumableArray3.default)(_this.requestMiddlewares.middlewares), (0, _toConsumableArray3.default)(reqMdw.reqMdw)),
+        params: (0, _extends4.default)({}, _this.requestMiddlewares.params, reqMdw.reqMdwParams)
+      };
       _this.wrappers[key].addRequestMiddlewares(reqMdw.reqMdw, reqMdw.reqMdwParams);
     }
     if (errMdw.errMdw) {
+      _this.errorMiddlewares = {
+        middlewares: [].concat((0, _toConsumableArray3.default)(_this.errMiddlewares.middlewares), (0, _toConsumableArray3.default)(errMdw.errMdw)),
+        params: (0, _extends4.default)({}, _this.errMiddlewares.params, errMdw.errMdwParams)
+      };
       _this.wrappers[key].addErrorMiddlewares(errMdw.errMdw, errMdw.errMdwParams);
     }
     _this.resources = (0, _extends4.default)((0, _defineProperty3.default)({}, key, _this.wrappers[key].routes), _this.resources);
   });
 
+  this.addWrappers = function (newWrappers) {
+    _this.wrappers = [].concat((0, _toConsumableArray3.default)(_this.wrappers), (0, _toConsumableArray3.default)(newWrappers));
+    if (_this.requestMiddlewares.middlewares && _this.requestMiddlewares.middlewares.length > 0) {
+      Object.keys(_this.wrappers).forEach(function (key) {
+        _this.wrappers[key].addRequestMiddlewares(_this.requestMiddlewares.middlewares, _this.requestMiddlewares.params);
+      });
+    }
+    if (_this.errorMiddlewares.middlewares && _this.errorMiddlewares.middlewares.length > 0) {
+      Object.keys(_this.wrappers).forEach(function (key) {
+        _this.wrappers[key].addErrorMiddlewares(_this.errorMiddlewares.middlewares, _this.errorMiddlewares.params);
+      });
+    }
+  };
+
   this.addRequestMiddlewares = function (middlewares, middlewareParams) {
+    _this.requestMiddlewares = {
+      middlewares: [].concat((0, _toConsumableArray3.default)(_this.requestMiddlewares.middlewares), (0, _toConsumableArray3.default)(middlewares)),
+      params: (0, _extends4.default)({}, _this.requestMiddlewares.params, middlewareParams)
+    };
     Object.keys(_this.wrappers).forEach(function (key) {
-      _this.wrappers[key].addRequestMiddlewares(middlewares, middlewareParams);
+      _this.wrappers[key].addRequestMiddlewares(_this.requestMiddlewares.middlewares, _this.requestMiddlewares.params);
     });
   };
 
   this.addErrorMiddlewares = function (middlewares, middlewareParams) {
+    _this.requestMiddlewares = {
+      middlewares: [].concat((0, _toConsumableArray3.default)(_this.requestMiddlewares.middlewares), (0, _toConsumableArray3.default)(middlewares)),
+      params: (0, _extends4.default)({}, _this.requestMiddlewares.params, middlewareParams)
+    };
     Object.keys(_this.wrappers).forEach(function (key) {
-      _this.wrappers[key].addErrorMiddlewares(middlewares, middlewareParams);
+      _this.wrappers[key].addErrorMiddlewares(_this.errorMiddlewares.middlewares, _this.errorMiddlewares.params);
     });
   };
 }
--- src/library.js
+++ src/library.js
@@ -1,27 +1,73 @@
-function CombineWrappers(wrappers,
+function CombineWrappers(
+  wrappers,
   reqMdw = { reqMdw: null, reqMdwParams: null },
-  errMdw = { errMdw: null, errMdwParams: null }) {
+  errMdw = { errMdw: null, errMdwParams: null },
+) {
   this.resources = {};
   this.wrappers = wrappers;
+  this.requestMiddlewares = { middlewares: [], params: undefined };
+  this.errorMiddlewares = { middlewares: [], params: undefined };
   Object.keys(this.wrappers).forEach((key) => {
     if (reqMdw.reqMdw) {
+      this.requestMiddlewares = {
+        middlewares: [...this.requestMiddlewares.middlewares, ...reqMdw.reqMdw],
+        params: { ...this.requestMiddlewares.params, ...reqMdw.reqMdwParams },
+      };
       this.wrappers[key].addRequestMiddlewares(reqMdw.reqMdw, reqMdw.reqMdwParams);
     }
     if (errMdw.errMdw) {
+      this.errorMiddlewares = {
+        middlewares: [...this.errMiddlewares.middlewares, ...errMdw.errMdw],
+        params: { ...this.errMiddlewares.params, ...errMdw.errMdwParams },
+      };
       this.wrappers[key].addErrorMiddlewares(errMdw.errMdw, errMdw.errMdwParams);
     }
     this.resources = { [key]: this.wrappers[key].routes, ...this.resources };
   });
 
+  this.addWrappers = (newWrappers) => {
+    this.wrappers = [...this.wrappers, ...newWrappers];
+    if (this.requestMiddlewares.middlewares && this.requestMiddlewares.middlewares.length > 0) {
+      Object.keys(this.wrappers).forEach((key) => {
+        this.wrappers[key].addRequestMiddlewares(
+          this.requestMiddlewares.middlewares,
+          this.requestMiddlewares.params,
+        );
+      });
+    }
+    if (this.errorMiddlewares.middlewares && this.errorMiddlewares.middlewares.length > 0) {
+      Object.keys(this.wrappers).forEach((key) => {
+        this.wrappers[key].addErrorMiddlewares(
+          this.errorMiddlewares.middlewares,
+          this.errorMiddlewares.params,
+        );
+      });
+    }
+  };
+
   this.addRequestMiddlewares = (middlewares, middlewareParams) => {
+    this.requestMiddlewares = {
+      middlewares: [...this.requestMiddlewares.middlewares, ...middlewares],
+      params: { ...this.requestMiddlewares.params, ...middlewareParams },
+    };
     Object.keys(this.wrappers).forEach((key) => {
-      this.wrappers[key].addRequestMiddlewares(middlewares, middlewareParams);
+      this.wrappers[key].addRequestMiddlewares(
+        this.requestMiddlewares.middlewares,
+        this.requestMiddlewares.params,
+      );
     });
   };
 
   this.addErrorMiddlewares = (middlewares, middlewareParams) => {
+    this.requestMiddlewares = {
+      middlewares: [...this.requestMiddlewares.middlewares, ...middlewares],
+      params: { ...this.requestMiddlewares.params, ...middlewareParams },
+    };
     Object.keys(this.wrappers).forEach((key) => {
-      this.wrappers[key].addErrorMiddlewares(middlewares, middlewareParams);
+      this.wrappers[key].addErrorMiddlewares(
+        this.errorMiddlewares.middlewares,
+        this.errorMiddlewares.params,
+      );
     });
   };
 }
